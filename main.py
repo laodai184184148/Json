5c69a489a92bdd290ee73e6dc44751ffe25cd31e
Đại Nguyễn Quốc <17521294@gm.uit.edu.vn>
2020-08-12 13:19:00 +0700

    second commit

--- main.py
+++ main.py
@@ -19,29 +19,25 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-cnx = mysql.connector.connect(user='l62WFY35bE', password='NluJ4Vh1ix',
-                                 host='remotemysql.com',
-                                 database='l62WFY35bE',port=3306)
+cnx = mysql.connector.connect(user='sql12359904', password='Uz363CqZFF',
+                                 host='sql12.freemysqlhosting.net',
+                                 database='sql12359904',port=3306)
 cursor = cnx.cursor()
 
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Chains",
-        "email": "alicechains@example.com",
-        "hashed_password": "$2b$12$gSvqqUPvlXP2tfVFaWK1Be7DlH.PKZbv5H8KnzzVgXXbVxpva.pFm",
-        "disabled": True,
-    },
-}
+def get_user_db(username:str):
+    cursor.execute("select *from user where email='"+username+"'")
 
+    myresult = cursor.fetchone()
+    
+    if myresult is None:
+        return None
+    return{
+        "username": myresult[1],
+        "hashed_password": myresult[2],
+        "disabled": False,
+        "role":myresult[3]
+        }
 
 class Token(BaseModel):
     access_token: str
@@ -55,8 +51,6 @@ class TokenData(BaseModel):
 
 class User(BaseModel):
     username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
     disabled: Optional[bool] = None
 
 
@@ -77,22 +71,14 @@ app = FastAPI()
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
+def authenticate_user(username: str, password: str):
+    user = get_user_db(username)
+    if user is None:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(password, user["hashed_password"]):
         return False
     return user
 
@@ -129,7 +115,7 @@ async def get_current_user(
         token_data = TokenData(scopes=token_scopes, username=username)
     except (JWTError, ValidationError):
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user_db(username=token_data.username)
     if user is None:
         raise credentials_exception
     for scope in security_scopes.scopes:
@@ -142,29 +128,38 @@ async def get_current_user(
     return user
 
 
-async def get_current_active_user(
-    current_user: User = Security(get_current_user, scopes=["me"])
-):
-    if current_user.disabled:
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user["disabled"]:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    new=get_user_db(form_data.username)
+    if new is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    if not verify_password(form_data.password, new["hashed_password"]):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username, "scopes": form_data.scopes},
-        expires_delta=access_token_expires,
+    access_token =create_access_token(
+        data={"sub": new["username"]}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.get("/users/me/", response_model=User)
+@app.get("/users/admin/", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    print(current_user)
     return current_user
 
 @app.get("/users/items/")
